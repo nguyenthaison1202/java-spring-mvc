681ed8e1fc09299fca1dda952727a682e029b89a
nguyenthaison <bffthaison@gmail.com>
2024-10-16 15:34:13 +0700

    update 16/10

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(
+		//disable security
+		exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+)
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
